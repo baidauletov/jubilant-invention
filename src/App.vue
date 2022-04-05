6b0abf3f46153d93426c4912c7c4363104ff598d
Baidauletov <baidauletov.nr@gmail.com>
2022-04-05 06:20:30 +0600

    Main monolith added

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,189 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <header class="header">
+      <span>Организационная структура</span>
+    </header>
+    <div style="margin: 1.25rem 1.5rem">
+      <button @click="addNode" class="add-button">+ Добавить</button>
+    </div>
+    <vue-tree-list
+      @click="onClick"
+      @change-name="onChangeName"
+      @delete-node="onDel"
+      @add-node="onAddNode"
+      @drop="drop"
+      @drop-before="dropBefore"
+      @drop-after="dropAfter"
+      :model="data"
+      default-tree-node-name="Отдел"
+      default-leaf-node-name="Отдел"
+      :default-expanded="false"
+    >
+      <template v-slot:leafNameDisplay="slotProps">
+        <span>
+          {{ slotProps.model.name }}
+        </span>
+      </template>
+      <template v-slot:addTreeNodeIcon="slotProps">
+        <span class="icon">{{ slotProps ? `📂` : '' }}</span>
+      </template>
+      <template v-slot:addLeafNodeIcon="slotProps">
+        <span class="icon">{{ slotProps ? `＋` : '' }}</span>
+      </template>
+      <template v-slot:editNodeIcon="slotProps">
+        <span class="icon">{{ slotProps ? `📃` : '' }}</span>
+      </template>
+      <template v-slot:delNodeIcon="slotProps">
+        <span class="icon">{{ slotProps ? `✂️` : '' }}</span>
+      </template>
+      <template v-slot:leafNodeIcon="slotProps">
+        <span class="icon">{{ slotProps ? `🍃` : '' }}</span>
+      </template>
+      <template v-slot:treeNodeIcon="slotProps">
+        <span class="icon">
+          {{
+            slotProps.model.children && slotProps.model.children.length > 0 && !slotProps.expanded
+              ? '🌲'
+              : ''
+          }}
+        </span>
+      </template>
+    </vue-tree-list>
   </div>
 </template>
 
 <script>
+import VueTreeList from './components/VueTreeList.vue'
+import { Tree, TreeNode } from './utils/Tree.js'
+
 export default {
-  name: 'App',
-  components: {},
+  components: {
+    VueTreeList,
+  },
+  data() {
+    return {
+      newTree: {},
+      data: new Tree([
+        {
+          name: 'Node 1',
+          id: 1,
+          pid: 0,
+          dragDisabled: true,
+          addTreeNodeDisabled: true,
+          addLeafNodeDisabled: true,
+          editNodeDisabled: true,
+          delNodeDisabled: true,
+          children: [
+            {
+              name: 'Node 1-2',
+              id: 2,
+              isLeaf: true,
+              pid: 1,
+            },
+          ],
+        },
+        {
+          name: 'Node 2',
+          id: 3,
+          pid: 0,
+          disabled: true,
+        },
+        {
+          name: 'Node 3',
+          id: 4,
+          pid: 0,
+        },
+      ]),
+    }
+  },
+  methods: {
+    onDel(node) {
+      // eslint-disable-next-line no-console
+      console.log(node)
+      node.remove()
+    },
+
+    onChangeName(params) {
+      // eslint-disable-next-line no-console
+      console.log(params)
+    },
+
+    onAddNode(params) {
+      // eslint-disable-next-line no-console
+      console.log(params)
+    },
+
+    onClick(params) {
+      // eslint-disable-next-line no-console
+      console.log(params)
+    },
+
+    drop: function ({ node, src, target }) {
+      // eslint-disable-next-line no-console
+      console.log('drop', node, src, target)
+    },
+
+    dropBefore: function ({ node, src, target }) {
+      // eslint-disable-next-line no-console
+      console.log('drop-before', node, src, target)
+    },
+
+    dropAfter: function ({ node, src, target }) {
+      // eslint-disable-next-line no-console
+      console.log('drop-after', node, src, target)
+    },
+
+    addNode() {
+      const node = new TreeNode({ name: 'new node', isLeaf: false })
+      if (!this.data.children) this.data.children = []
+      this.data.addChildren(node)
+    },
+
+    getNewTree() {
+      const vm = this
+      function _dfs(oldNode) {
+        const newNode = {}
+
+        for (const k in oldNode) {
+          if (k !== 'children' && k !== 'parent') {
+            newNode[k] = oldNode[k]
+          }
+        }
+
+        if (oldNode.children && oldNode.children.length > 0) {
+          newNode.children = []
+          for (let i = 0, len = oldNode.children.length; i < len; i++) {
+            newNode.children.push(_dfs(oldNode.children[i]))
+          }
+        }
+        return newNode
+      }
+
+      vm.newTree = _dfs(vm.data)
+    },
+  },
 }
 </script>
 
 <style lang="scss">
+@import './assets/style.scss';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 4.5rem 6rem;
+}
+
+.header {
+  color: $white;
+  background-color: $base-color;
+  text-transform: uppercase;
+  display: flex;
+  padding: 1.25rem;
+}
+
+.add-button {
+  cursor: pointer;
 }
 </style>
---
+++ src/utils/tools.js
@@ -0,0 +1,40 @@
+var handlerCache
+
+export const addHandler = function (element, type, handler) {
+  handlerCache = handler
+  if (element.addEventListener) {
+    element.addEventListener(type, handler, false)
+  } else if (element.attachEvent) {
+    element.attachEvent('on' + type, handler)
+  } else {
+    element['on' + type] = handler
+  }
+}
+
+export const removeHandler = function (element, type) {
+  if (element.removeEventListener) {
+    element.removeEventListener(type, handlerCache, false)
+  } else if (element.detachEvent) {
+    element.detachEvent('on' + type, handlerCache)
+  } else {
+    element['on' + type] = null
+  }
+}
+
+export const traverseTree = (root) => {
+  const newRoot = {}
+
+  for (const node in root) {
+    if (node !== 'children' && node !== 'parent') {
+      newRoot[node] = root[node]
+    }
+  }
+
+  if (root.children && root.children.length > 0) {
+    newRoot.children = []
+    for (let i = 0, len = root.children.length; i < len; i++) {
+      newRoot.children.push(traverseTree(root.children[i]))
+    }
+  }
+  return newRoot
+}
